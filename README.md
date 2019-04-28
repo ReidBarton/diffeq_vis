414e81d73b7ca317c7d9ff910a5cea4a2698b8a4
Reid Barton <reidabarton@gmail.com>
2019-04-27 21:01:28 -0400

    Live Input and code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # diffeq_vis
 demo here https://reidbarton.github.io/diffeq_vis/
+
+Uses p5.js library to visualize differential equations. 
--- diff_eq.js
+++ diff_eq.js
@@ -4,7 +4,7 @@ var speed;
 var stars = 400;
 
 
-var time = 0;
+var t = 0;
 var noiseScale=0.01;
 var lifetime = 300;
 var h = 0.01;
@@ -15,44 +15,14 @@ var x_off_old = 0;
 var y_off_old = 0;
 var soundon = false;
 
+
+var dx_input;
+var dy_input;
+
 var mouse_clk_x = 0;
 var mouse_clk_y = 0;
 
 let osc, osc2;
-
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent('sketch-holder');
-  speed = 10;
-  zoom_scale = 1/width
-  background(0);
-  for(var i=0; i<stars; i++){
-    points.push(new Point((random()- 1/2)*width*zoom_scale, (random()- 1/2)*height*zoom_scale));
-    stroke(255);
-    // osc_temp = new p5.Oscillator()
-    // osc_temp.amp(0.5);
-    // osc.push(osc_temp);
-
-  }
-  osc = new p5.Oscillator();
-  osc2 = new p5.Oscillator();
-  //zoom_scale = 504428960.9570371
-
-}
-
-
-function mouseDragged() {
-  x_off = mouseX - mouse_clk_x + x_off_old;
-  y_off = mouseY - mouse_clk_y + y_off_old;
-  // x_off = mouseX - pmouseX + x_off_old;
-  // y_off = mouseY - mouse_clk_y + y_off_old;
-  // x_off_old = 0;
-  // y_off_old = 0;
-  console.log(x_off)
-
-  return false;
-}
-
 
 class Point {
   constructor(x, y) {
@@ -65,33 +35,70 @@ class Point {
 }
 
 
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('sketch-holder');
+  speed = 10;
+  zoom_scale = 1/200;
+  background(0);
+  for(var i=0; i<stars; i++){
+    points.push(new Point((random()- 1/2)*width*zoom_scale, (random()- 1/2)*height*zoom_scale));
+    stroke(255);
+    // osc_temp = new p5.Oscillator()
+    // osc_temp.amp(0.5);
+    // osc.push(osc_temp);
+
+  }
+  // osc = new p5.Oscillator();
+  // osc2 = new p5.Oscillator();
+  //zoom_scale = 504428960.9570371
+  updateFunction()
+
+}
+
+
+// function mouseDragged() {
+//   x_off = mouseX - mouse_clk_x + x_off_old;
+//   y_off = mouseY - mouse_clk_y + y_off_old;
+//   // x_off = mouseX - pmouseX + x_off_old;
+//   // y_off = mouseY - mouse_clk_y + y_off_old;
+//   // x_off_old = 0;
+//   // y_off_old = 0;
+
+//   return false;
+// }
+
+
+
 function mouseWheel(event) {
   zoom_scale += zoom_scale/1000*event.delta;
+  document.getElementById("zoom").innerHTML = str(100*zoom_scale).substring(0,7);
+
 }
 
-function mouseClicked() {
-  // for(var i=0; i<stars; i++){
-  //   osc[i].start();
-  // }
-  if(soundon){
-    osc.amp(0);
-    soundon = false;
-    osc2.amp(0);
-  } else {
-    // osc.start();  
-    // osc2.start();
-    osc.amp(0.5);
-    osc2.amp(0.5);
-    soundon = true;
-  }
-}
+// function mouseClicked() {
+//   // for(var i=0; i<stars; i++){
+//   //   osc[i].start();
+//   // }
+//   if(soundon){
+//     osc.amp(0);
+//     soundon = false;
+//     osc2.amp(0);
+//   } else {
+//     // osc.start();  
+//     // osc2.start();
+//     osc.amp(0.5);
+//     osc2.amp(0.5);
+//     soundon = true;
+//   }
+// }
 
-function mousePressed(){
-  mouse_clk_x = mouseX;
-  mouse_clk_y = mouseY;
-  x_off_old = x_off;
-  y_off_old = y_off;
-}
+// function mousePressed(){
+//   mouse_clk_x = mouseX;
+//   mouse_clk_y = mouseY;
+//   x_off_old = x_off;
+//   y_off_old = y_off;
+// }
 
 
 function keyTyped(){
@@ -102,89 +109,28 @@ function keyTyped(){
   }
 }
 
-function draw() {
-  //stroke("#88aaff");
-  
 
+function draw() {  
+
+  // draw semi-transparent background
   background(0,10);
 
   for(var i=0; i<stars; i++){
 
-    // update pos using forward Euler
+    var x = points[i].px;
+    var y = points[i].py;
 
-    if(time<7) {
-      xspeed = h*points[i].py;
-      yspeed = h*points[i].px*cos(points[i].py*time);
-    } else if(time<14) {
-      xspeed = h*points[i].py*sin(points[i].py);
-      yspeed = h*points[i].px*cos(points[i].py);
-    } else if(time<20) {
-      xspeed = h*points[i].py*log(abs(points[i].py));
-      yspeed = h*points[i].px*cos(points[i].py);
-    } else if(time<25){
-      // skylar "gas gas gas"; h = 0.01
-      xspeed = h*cos(time)*(1-cos(time));
-      yspeed = h*points[i].px/points[i].py;
-      if(points[i].px/points[i].py > 0.5){
-        yspeed = 0;
-      }
-    } else if(time<30) {
-      // super-sonic hyway
-      xspeed = h*2.71^(-points[i].py)*points[i].px/points[i].py;
-      yspeed = h*(cos(points[i].px) + (sin(points[i].py))^2);
-    } else if(time<35) {
-      // pool-boi h=0.01
-      xspeed = h*(cos(points[i].px)+cos(points[i].py));
-      yspeed = h*((points[i].px)^2 + (points[i].py)^2)*cos(time);
-    } else if(time<40) {
-      // spiral boi (put negs for zoom change), h = 0.04
-      xspeed = -1*h*(points[i].py + points[i].px);
-      yspeed = -1*h*(points[i].py - points[i].px);
-    } else if(time<45) {
-      xspeed = h*points[i].py;
-      yspeed = h*points[i].px*cos(points[i].py*time);
-    } else if(time<55) {
-      // spirals in squares 
-      zoom_scale = 504428960.9570371; //h = 0.03; lifetime = 72
-      xspeed = 1*h*cos(time)*(points[i].py + points[i].px)^2;
-      yspeed = 1*h*cos(time)*(points[i].py - points[i].px)^2;
-    } else {
-      time = 0;
-      zoom_scale = 1/width;
-    }
-    
-
-
-
-
-
-
-
-    
-
-   
-
-
-
-    
-    // xspeed = h*points[i].py*(points[i].px);
-    // yspeed = h*points[i].py*points[i].py;
-
-
+    xspeed = eval(dx_input);
+    yspeed = eval(dy_input);
 
     // update using forward Euler approximation 
-    points[i].xpos = points[i].px + xspeed;
-    points[i].ypos = points[i].py + yspeed;
+    points[i].xpos = x + xspeed;
+    points[i].ypos = y + yspeed;
 
     speed = sqrt(h*points[i].py*h*points[i].py + h*points[i].px*h*points[i].px)
 
-    stroke(150-100*xspeed/speed, 150+speed*60, 150+ 100*yspeed/speed);
-    // stroke(30*xspeed/speed+100, speed*200+100, 200+ 70*yspeed/speed);
-    // stroke(255);
+    stroke(150-100*xspeed/speed, 150+speed*60, 150+ 100*yspeed/speed); // color
     line(points[i].xpos/zoom_scale + x_off + width/2, -1*points[i].ypos/zoom_scale + y_off+ height/2, points[i].px/zoom_scale + x_off + width/2, -1*points[i].py/zoom_scale + y_off + height/2);
-
-
-
 
 
     points[i].py = points[i].ypos;
@@ -204,8 +150,95 @@ function draw() {
     
   }
 
-  osc.freq(xspeed*500000);    
-  osc2.freq(yspeed*600000);
-  time+=h;
-  //console.log(zoom_scale)
+  // osc.freq(xspeed*500000);    
+  // osc2.freq(yspeed*600000);
+  t+=h;
+}
+
+
+function updateFunction(){
+  var dx = document.getElementById("dx");
+  var dy = document.getElementById("dy");
+
+  var newdx = "" + str(dx.value);
+  newdx = "h*(" + newdx + ")";
+
+  var newdy = "" + str(dy.value);
+  newdy = "h*(" + newdy + ")";
+
+  dx_input = newdx;
+  dy_input = newdy;
+}
+
+
+function updateRandom(){
+  var dx = document.getElementById("dx");
+  var dy = document.getElementById("dy");
+
+  rnum = Math.random()*13;
+
+  if(rnum<1) {
+    dx_input = "y+x";
+    dy_input = "x*cos(y*t)";
+    zoom_scale = 1/100;
+  } else if(rnum<2) {
+    dx_input = "y*sin(y)";
+    dy_input = "x*cos(y)";
+    zoom_scale = 1/100;
+  } else if(rnum<3) {
+    dx_input = "y*log(abs(y))";
+    dy_input = "x*cos(y)";
+    zoom_scale = 0.7/100;
+  } else if(rnum<4){
+    dx_input = "cos(x)*5";
+    dy_input = "sin(y)*5";
+    zoom_scale = 1/100;
+  }
+  else if(rnum<5) {
+    // super-sonic hyway
+    dx_input = "2.71^(-y)*x/y";
+    dy_input = "cos(x) + (sin(y)^2)";
+    zoom_scale = 1/100;
+  } else if(rnum<6) {
+    dx_input = "cos(x)+cos(y)";
+    dy_input = "(x^2 + y^2)*cos(t)";
+    zoom_scale = 1.43/100;
+  } else if(rnum<7) {
+    dx_input = "-1*(y + x)";
+    dy_input = "-1*(y - x)";
+    zoom_scale = 1/100;
+  } else if(rnum<8) {
+    dx_input = "y";
+    dy_input = "x*cos(y*t)";
+    zoom_scale = 0.04/100;
+  } else if(rnum<9) {
+    // spirals in squares 
+    zoom_scale = 9000088; //h = 0.03; lifetime = 72
+    dx_input = "cos(t)*(y + x)^2";
+    dy_input = "cos(t)*(y - x)^2";
+  } else if(rnum<10) {
+    dx_input = "y*x";
+    dy_input = "y*y";
+    zoom_scale = 1/100;
+  } else if(rnum<11) {
+    dx_input = "tan(t)/(abs(x) + abs(y))*(x*cos(t/3) + y*sin(t/3))";
+    dy_input = "cos(t)/(abs(x) + abs(y))*(y*cos(t/3) + x*sin(t/3))";
+    zoom_scale = 0.05/100;
+  } else if(rnum<12) {
+    dx_input = "sin(x + y^2)";
+    dy_input = "cos(y + x^2)";
+    zoom_scale = 1/100;
+  } else if(rnum<13) {
+    dx_input = "sin(x^2 + y^2)";
+    dy_input = "cos(y^2 + x^2)";
+    zoom_scale = 1/100;
+  }
+  
+  dx.value = dx_input;
+  dy.value = dy_input;
+  dx_input = "h*(" + dx_input + ")"
+  dy_input = "h*(" + dy_input + ")"
+  document.getElementById("zoom").innerHTML = str(100*zoom_scale).substring(0,7);
+
+
 }
